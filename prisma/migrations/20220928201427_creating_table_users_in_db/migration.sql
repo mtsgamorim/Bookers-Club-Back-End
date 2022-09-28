68887afffd00bdd96ca6a5a14e31b8403de21c5e
Matheus Amorim <mtsgamorim@gmail.com>
2022-09-28 17:17:54 -0300

    Creating BD with table users

---
+++ prisma/migrations/20220928201427_creating_table_users_in_db/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
