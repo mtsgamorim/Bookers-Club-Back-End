59ecac057fef1a5440373c19781374bc268d7b2e
Matheus Amorim <mtsgamorim@gmail.com>
2022-10-10 01:29:35 -0300

    fix deploy bug

--- tests/integration/users.test.ts
+++ tests/integration/users.test.ts
@@ -17,4 +17,33 @@ describe("Testes na rota Post: /sign-up", () => {
     const result = await supertest(app).post("/sign-up").send(user);
     expect(result.status).toEqual(201);
   });
+  it("Caso erro: Email ja cadastrado, retornar 409", async () => {
+    const user = userFactory();
+    await supertest(app).post("/sign-up").send(user);
+    const result = await supertest(app).post("/sign-up").send(user);
+    expect(result.status).toEqual(409);
+  });
+});
+
+describe("Testes na rota Post: /sign-in", () => {
+  it("Caso sucesso: retornar status 200", async () => {
+    const user = userFactory();
+    await supertest(app).post("/sign-up").send(user);
+    const data = {
+      email: user.email,
+      password: user.password,
+    };
+    const result = await supertest(app).post("/sign-in").send(data);
+    expect(result.status).toEqual(200);
+  });
+
+  it("Caso erro: retornar status 401", async () => {
+    const user = userFactory();
+    const data = {
+      email: user.email,
+      password: user.password,
+    };
+    const result = await supertest(app).post("/sign-in").send(data);
+    expect(result.status).toEqual(401);
+  });
 });
